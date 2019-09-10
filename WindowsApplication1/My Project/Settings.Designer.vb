4bccd99065e2a0425fa94c7556c0e5e23ff69c2c
Ej1224 <39943828+Ej1224@users.noreply.github.com>
2019-09-10 17:27:04 +0800

    5:26PM 09.10.2019 *Switching to .mdf database

--- WindowsApplication1/My Project/Settings.Designer.vb
+++ WindowsApplication1/My Project/Settings.Designer.vb
@@ -53,6 +53,17 @@ Namespace My
                 Return defaultInstance
             End Get
         End Property
+        
+        <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\mcgi.mdf;Inte"& _ 
+            "grated Security=True")>  _
+        Public ReadOnly Property mcgiConnectionString() As String
+            Get
+                Return CType(Me("mcgiConnectionString"),String)
+            End Get
+        End Property
     End Class
 End Namespace
 
